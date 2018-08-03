850b3e1ae90c316adff15f172efcd6236692fe3d
Paul <paulfroelich97@gmail.com>
2018-08-03 16:17:50 +0200

    wrote my first complete keymap

--- keyboards/katana60/keymaps/icy0/keymap.c
+++ keyboards/katana60/keymaps/icy0/keymap.c
@@ -35,34 +35,61 @@
 #define SYMB 2 // Symbols
 #define CURS 3 // Text Editing
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+/*
+ * DF(layer) - switches the default layer. The default layer is the always-active base layer that other layers stack on top of. 
+ * See below for more about the default layer. This 	might be used to switch from QWERTY to Dvorak layout. 
+ * (Note that this is a temporary switch that only persists until the keyboard loses power. 
+ * To modify the default layer in a persistent way requires deeper customization, such as calling the set_single_persistent_default_layer function inside of process_record_user.)
+ * 
+ * MO(layer) - momentarily activates layer. As soon as you let go of the key, the layer is deactivated.
+ * 
+ * LM(layer, mod) - Momentarily activates layer (like MO), but with modifier(s) mod active. Only supports layers 0-15 and the left modifiers.
+ * 
+ * LT(layer, kc) - momentarily activates layer when held, and sends kc when tapped.
+ * 
+ * TG(layer) - toggles layer, activating it if it's inactive and vice versa
+ * 
+ * TO(layer) - activates layer and de-activates all other layers (except your default layer). 
+ * This function is special, because instead of just adding/removing one layer to your active layer stack, 
+ * it will completely replace your current active layers, uniquely allowing you to replace higher layers with a lower one. 
+ * This is activated on keydown (as soon as the key is pressed).
+ * 
+ * TT(layer) - Layer Tap-Toggle. If you hold the key down, layer is activated, and then is de-activated when you let go (like MO). 
+ * If you repeatedly tap it, the layer will be toggled on or off (like TG). It needs 5 taps by default, 
+ * but you can change this by defining TAPPING_TOGGLE -- for example, #define TAPPING_TOGGLE 2 to toggle on just two taps.
+ 
+ */
+
+// _______ == KC_TRNS
+ 
+ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT( /* Base */
-  KC_ESC,  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    DF(1),   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_PLUS,
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,          KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-  MO(3),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_HOME,          KC_PGUP, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_END,  KC_DEL,  KC_PGDN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-  MO(2),   KC_LCTL, KC_LGUI, KC_LALT,                   KC_BSPC, KC_ENT,  K_SPCFN,          KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,MO(2)
+  KC_ESC,  KC_BSLS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    TO(1),   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_PLUS,
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,          KC_RBRC, KC_Z,    KC_U,    KC_I,    KC_O,    KC_P,    KC_SLSH,
+  MO(3),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_HOME,          KC_PGUP, KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT, KC_LALT,
+  KC_LSFT, KC_Y,    KC_X,    KC_C,    KC_V,    KC_B,    KC_END,  KC_DEL,  KC_PGDN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SCLN, KC_RSFT,
+  KC_LCTL, KC_LGUI, KC_LALT, MO(2),            KC_SPC,  		 KC_ENT,  KC_BSPC,          KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_APP
     ),
 [NUMB] = LAYOUT(
-  _______, _______, _______, _______, _______, _______, _______, DF(0),   KC_PSLS, KC_PAST, KC_PMNS, _______, _______, _______, _______,
-  _______, _______, _______, KC_MS_U, _______, _______, _______,          KC_7,    KC_8,    KC_9,    KC_PPLS, _______, _______, _______,
-  _______, KC_BTN2, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______,          KC_4,    KC_5,    KC_6,    KC_PPLS, _______, _______, _______,
-  _______, _______, KC_ACL0, KC_ACL1, KC_ACL2, _______, _______, _______, KC_1,    KC_2,    KC_3,    KC_PENT, _______, _______, _______,
-  _______, _______, _______, _______,                   KC_BTN1, _______, KC_P0,            KC_PDOT, KC_PENT, _______, _______, _______
+         ,        ,        ,        ,        ,        ,        , DF(0),       ,        , KC_PSLS, KC_PAST,    KC_PMNS,           ,        ,
+         ,        ,        , KC_MS_U,        ,        ,        ,              ,    KC_7,    KC_8,    KC_9,    KC_PPLS,        ,        ,
+         , KC_BTN2, KC_MS_L, KC_MS_D, KC_MS_R,        ,        ,              ,    KC_4,    KC_5,    KC_6,    KC_PCMM,        ,        , 
+         ,        , KC_ACL0, KC_ACL1, KC_ACL2,        ,        ,      ,       ,    KC_1,    KC_2,    KC_3,    KC_PENT,        ,        ,
+		 , 	      ,        ,        ,                   KC_BTN1,      ,       ,   KC_P0, KC_PDOT, KC_PENT,           ,
     ),
 [SYMB] = LAYOUT(
-  RESET,   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______,          _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
-  _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_VOLD,          KC_VOLU, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
-  _______, KC_PLUS, KC_MINS, KC_EQL,  KC_LCBR, KC_RCBR, KC_MPRV, KC_MPLY, KC_MNXT, KC_LBRC, KC_RBRC, KC_SCLN, KC_COLN, KC_BSLS, _______,
-  _______, _______, _______, _______,                   KC_DEL,  _______, _______,          _______, _______, _______, _______, _______
+    RESET,          ,     KC_F1,     KC_F2,   KC_F3,   KC_F4,   KC_F5,          , KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+   KC_GRV,   KC_CIRC,          ,          ,        ,        ,        ,                 ,        ,        ,        ,        ,        ,        ,
+         ,UNICODE(Ü),UNICODE(Ö),UNICODE(Ä),        ,        , KC_VOLD,          KC_VOLU,        ,        ,        ,        ,        ,        ,
+         ,          ,          ,          ,        ,        , KC_MPRV, KC_MPLY, KC_MNXT,        ,        ,        ,        ,        ,        ,
+         ,          ,          ,          ,                          ,        ,        ,                 ,        ,        ,        ,        
     ),
 [CURS] = LAYOUT(
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-  _______, _______, KC_WH_U, _______, _______, _______, _______,          _______, _______, K_PRVWD, KC_UP,   K_NXTWD, _______, _______,
-  _______, KC_LCTL, KC_WH_D, KC_LSFT, _______, _______, _______,          _______, KC_BSPC, KC_LEFT, KC_DOWN, KC_RIGHT,KC_DEL,  _______,
-  _______, UNDO,    CUT,     COPY,    PASTE,   _______, _______, _______, _______, _______, K_LSTRT, _______, K_LEND,  _______, _______,
-  _______, _______, _______, _______,                   _______, _______, _______,          _______, _______, _______, _______, _______
+         ,        ,        ,        ,        ,        ,        ,        ,        ,        ,        ,        ,        ,        ,        ,
+         ,        , KC_WH_U,        ,        ,        ,        ,                 ,        , K_PRVWD, KC_UP,   K_NXTWD,        ,        ,
+         , KC_LCTL, KC_WH_D, KC_LSFT,        ,        ,        ,                 , K_LSTRT, KC_LEFT, KC_DOWN, KC_RIGHT, K_LEND,        ,
+         , UNDO,    CUT,     COPY,    PASTE,          ,        ,        ,        ,        , KC_BSPC,        , KC_DEL,         ,        ,
+         ,        ,        ,        ,                          ,        ,        ,                 ,        ,       ,         ,        
     )
 
 };
